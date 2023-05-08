d7c756870d3f52580dfbbb8628b47d5a562661aa
Colinchang123 <S10242330@connect.np.edu.sg>
2023-05-08 21:34:09 +0800

    Revert "1"
    This reverts commit bcd6e8004db42003f4aec28ee1f8e344b69b8d6e.

--- app/src/main/java/com/example/week2practical/ListActivity.java
+++ app/src/main/java/com/example/week2practical/ListActivity.java
@@ -2,8 +2,6 @@ package com.example.week2practical;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,7 +10,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
@@ -22,17 +19,8 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        Random random = new Random();
-        ArrayList<User> Users20 = new ArrayList<>();
-        for(int i = 0; i < 20; i++){
-            Users20.add(new User("Name" + random.nextInt(), "Description" + random.nextInt(), random.nextInt(), random.nextBoolean()));
-        }
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new Adapter(getApplicationContext(), Users20));
-
         ImageView imgButton = findViewById(R.id.imageView2);
+
         imgButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 new AlertDialog.Builder(ListActivity.this)
